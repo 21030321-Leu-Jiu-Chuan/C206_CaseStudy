9bebf4201931b65e02fcd8236143991d07261be6
21011378 <21011378@10.174.124.5>
2022-08-08 11:48:55 +0800

    Update

--- C206_CaseStudy/src/MenuItem.java
+++ C206_CaseStudy/src/MenuItem.java
@@ -14,15 +14,7 @@
  * @author 21011378
  *
  */
-=======
 
-/**
- * I declare that this code was written by me.
- * I will not copy or allow others to copy my code.
- * I understand that copying code is considered as plagiarism.
- *
- * 21030321 Leu Jiu Chuan, 8 Aug 2022 11:39:51 am
- */
 
 //github.com/21030321-Leu-Jiu-Chuan/C206_CaseStudy.git
 public class MenuItem {
