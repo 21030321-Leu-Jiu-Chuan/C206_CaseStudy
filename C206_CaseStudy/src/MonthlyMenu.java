3b073a2fd218800765d1e293421c25e49ba310c4
21011378 <21011378@10.174.124.5>
2022-08-08 11:38:42 +0800

    Classes

---
+++ C206_CaseStudy/src/MonthlyMenu.java
@@ -0,0 +1,18 @@
+/*
+* I declare that this code was written by me. 
+ * I do not copy or allow others to copy my code. 
+ * I understand that copying code is considered as plagiarism.
+ * 
+ * Student Name:Sherman Poon
+ * Student ID: 21011378
+ * Class: C208
+ * Date/Time created: Monday 08-08-2022 11:37
+ */
+
+/**
+ * @author 21011378
+ *
+ */
+public class MonthlyMenu {
+
+}
