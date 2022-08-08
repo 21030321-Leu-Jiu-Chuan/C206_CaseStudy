4a363b865ff5246eaf8b655dc8a528e0c7124eb7
21030321 <21030321@10.174.123.100>
2022-08-08 11:40:04 +0800

    class

---
+++ C206_CaseStudy/src/MenuItem.java
@@ -0,0 +1,12 @@
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 21030321 Leu Jiu Chuan, 8 Aug 2022 11:39:51 am
+ */
+
+public class MenuItem {
+
+}
